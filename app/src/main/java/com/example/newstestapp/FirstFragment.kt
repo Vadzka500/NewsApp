420d47297d5233bee017f845021a392c7a947dbd
Vadzka500 <Vad-grodno@mail.ru>
2023-01-19 12:52:51 +0300

    -

---
+++ app/src/main/java/com/example/newstestapp/FirstFragment.kt
@@ -0,0 +1,48 @@
+package com.example.newstestapp
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.newstestapp.databinding.FragmentFirstBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        Log.i("main", "start fragment")
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+
+        //_binding!!.firstCard.shimmer.startShimmer()
+        _binding!!.root.startShimmer()
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+       /* binding.buttonFirst.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }*/
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
